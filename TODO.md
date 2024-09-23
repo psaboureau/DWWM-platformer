8036f1a96917c6c72abd7c946bfd87602b746d83
psaboureau <psaboureau@gmx.fr>
2024-09-23 16:44:23 +0200

    Collision des plateformes + TODO.md added

---
+++ TODO.md
@@ -0,0 +1,12 @@
+TODO: 
+
+Platform
+Collision Detection
+Scroll the background
+Win scenario
+Image platforms
+Parallax scrolls
+Death Pits
+Level Creation
+Fine tuning
+Sprite creation
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const c = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const gravity = 1.5;
+
+const gravity = 0.5;
 
 class Player {
   constructor() {
@@ -28,52 +29,80 @@ class Player {
 
   update() {
     this.draw();
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
 
     // Applique la gravité seulement quand le joueur est en l'air
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity; // Tire (pousse) le joueur vers le bas
     } else {
-      // Arrête le joueur au fond
+      // Arrête le joueur quand il touche le bas du canvas
       this.velocity.y = 0;
     }
   }
 }
 
-const player = new Player();
+// Définition d'un classe plateforme avec un position(x, y), un largeur(width) et une hauteur(height)
+class Platform {
+  constructor() {
+    this.position = {
+      x: 200,
+      y: 300,
+    };
+    this.width = 200;
+    this.height = 20;
+  }
 
-const keys = {
-    right: {
-      pressed: false
-    }, 
-    left: {
-      pressed: false
-    }
+  draw() {
+    c.fillStyle = "blue";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
 }
 
+// Instanciate les class Player et Plateform
+const player = new Player();
+const platform = new Platform();
+
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  platform.draw();
 
-    if (keys.right.pressed) {
-      player.velocity.x = 5;
-    } else if (keys.left.pressed) {
-      player.velocity.x = -5;
-    } else {
-      player.velocity.x = 0;
-    }
+  // Si les touches Q ou D sont enfoncés déplacent le joueur vers la gauche ou la droite
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
 
+  // Gère la collision avec les plateformes
+  // ( collision présente du haut vers le bas mais pas l'inverse)
+  if (
+    player.position.y + player.height <= platform.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform.position.y &&
+    player.position.x + player.width >= platform.position.x &&
+    player.position.x <= platform.position.x + platform.width
+  ) {
+    player.velocity.y = 0;
+  }
 }
 
 animate();
 
-
-// Mouvement du joueur
+// Ecoute si les touches ZQSD sont enfoncés
 window.addEventListener("keydown", ({ keyCode }) => {
   // console.log(keyCode);
   switch (keyCode) {
@@ -86,17 +115,16 @@ window.addEventListener("keydown", ({ keyCode }) => {
       break;
     case 68:
       console.log("right");
-      keys.right.pressed = true;                                     
+      keys.right.pressed = true;
       break;
     case 90:
       console.log("up");
-      player.velocity.y -= 20
+      player.velocity.y -= 30;
       break;
   }
 });
 
-
-// Mouvement du joueur
+// Ecoute si les touches ZQSD sont relachés
 window.addEventListener("keyup", ({ keyCode }) => {
   // console.log(keyCode);
   switch (keyCode) {
@@ -109,7 +137,7 @@ window.addEventListener("keyup", ({ keyCode }) => {
       break;
     case 68:
       console.log("right");
-      keys.right.pressed = false;                                 
+      keys.right.pressed = false;
       break;
     case 90:
       console.log("up");
